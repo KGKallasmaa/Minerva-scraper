bfb6fbf06f760a14a8da7427f87b1a3575c41710
karl96k <karl.gustav1789@gmail.com>
2019-12-29 17:50:50 +0200

    new microservice

---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+import threading
+
+from scraper.main import start_scraper
+
+
+def scraper():
+    start_scraper()
+
+
+from pageRank.page_rank import rank
+
+
+def pageRank():
+    start = time.process_time()
+    print("Starting to calculate pageRank")
+    rank()
+    end = time.process_time()
+    print("Pagerank calculation completed in", (end - start), "seconds.")
+
+
+if __name__ == '__main__':
+
+
+    threads = []
+
+    ## Starting the scraper
+
+    thread1 = threading.Thread(target=scraper)
+    #threads.append(thread1)
+
+    ## Starting the pageranker
+
+    thread2 = threading.Thread(target=pageRank)
+    threads.append(thread2)
+
+    for t in threads:
+        t.start()
