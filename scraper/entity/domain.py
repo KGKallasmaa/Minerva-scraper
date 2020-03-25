3445f73aba342531fa6a37508cebe448f67c3512
KGKallasmaa <karl.gustav1789@gmail.com>
2020-03-25 12:08:17 +0200

    add more fileds to domain entity

--- scraper/entity/domain.py
+++ scraper/entity/domain.py
@@ -1,3 +1,6 @@
+import tldextract
+
+
 class Domain:
     def __init__(self, domain, favicon, current_time):
         self.domain = domain
@@ -6,6 +9,7 @@ class Domain:
         self.last_crawl_UTC = current_time
         self.sitemap_is_present = True
         self.ssl_is_present = "https://" in domain
+        self.domain_type = self.get_domain_type()
 
     def get_values_for_db(self):
         return {
@@ -14,5 +18,10 @@ class Domain:
             "first_crawl_UTC": self.first_crawl_UTC,
             "last_crawl_UTC": self.last_crawl_UTC,
             "sitemap_is_present": self.sitemap_is_present,
-            "ssl_is_present": self.ssl_is_present
+            "ssl_is_present": self.ssl_is_present,
+            "domain_type": self.domain_type
         }
+
+    def get_domain_type(self):
+        ext = tldextract.extract(self.domain)
+        return ext.suffix
