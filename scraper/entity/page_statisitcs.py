7cb6c8761ac74c08a9c307ae5c1fc087a5cefb5d
KGKallasmaa <karl.gustav1789@gmail.com>
2020-03-28 03:08:54 +0200

    support Hub and Authority scores

--- scraper/entity/page_statisitcs.py
+++ scraper/entity/page_statisitcs.py
@@ -1,25 +1,27 @@
 import itertools
 
-import pyhash
 import readability
 
-from scraper.database.database import get_page_rank_by_page_id
 from scraper.entity.language import Language
 from scraper.scraping.scraper import get_domain
-from scraper.utils.utils import de_compress, compress_urls, merge_dictionaries
-
-fp = pyhash.farm_fingerprint_64()
+from scraper.utils.utils import de_compress, compress_urls, merge_dictionaries, get_fingerprint_from_raw_data
 
 
 class PageStatistics:
     def __init__(self, page_id, current_time, page, speed, client):
         self.page_id = page_id
         self.update_date_UTC = current_time
+
         self.language = self.calculate_language_statistics(page)
         self.named_entities = self.calculate_named_entity(page)
-        self.page_rank = get_page_rank_by_page_id(self.page_id, client=client)
+
+        self.page_rank = self.get_page_rank_by_page_id(self.page_id, client=client)
+        self.hub_score = self.get_hub_score_by_page_id(self.page_id, client=client)
+        self.authority_score = self.get_authority_score_by_page_id(self.page_id, client=client)
+
         self.page_load_speed = speed
         self.url_length = len(page.url)
+
         self.url_is_canonical = page.url_is_canonical
 
         self.words_in_h1 = self.get_words_in_headings(page.heading1)
@@ -43,18 +45,29 @@ class PageStatistics:
             "update_date_UTC": current_time,
             "language": self.language,
             "named_entities": self.named_entities,
-            "pageRank": self.page_rank,
+            "pageRank": self.page_rank,  # TODO: delete duplicate. Make sure ranking and search dont' break :)
+            "scores": {
+                "pageRank": self.page_rank,
+                "hub_score": self.hub_score,
+                "authority_score": self.authority_score,
+            },
             "page_load_speed": self.page_load_speed,
             "url_length": self.url_length,
-            "words_in_heading1": self.words_in_h1,
-            "words_in_heading2": self.words_in_h2,
-            "words_in_heading3": self.words_in_h3,
-            "nr_of_links_from_gov": self.nr_of_links_from_gov,
-            "nr_of_links_from_edu": self.nr_of_links_from_edu,
-            "nr_of_links_from_org": self.nr_of_links_from_org,
-            "nr_of_links_to_gov": self.nr_of_links_to_gov,
-            "nr_of_links_to_edu": self.nr_of_links_to_edu,
-            "nr_of_links_to_org": self.nr_of_links_to_org,
+            "words_in_headings": {
+                "heading1": self.words_in_h1,
+                "heading2": self.words_in_h2,
+                "heading3": self.words_in_h3,
+            },
+            "nr_links_from_GEO": {
+                "from_gov": self.nr_of_links_from_gov,
+                "from_edu": self.nr_of_links_from_edu,
+                "from_org": self.nr_of_links_from_org,
+            },
+            "nr_links_to_GEO": {
+                "to_gov": self.nr_of_links_to_gov,
+                "to_edu": self.nr_of_links_to_edu,
+                "to_org": self.nr_of_links_to_org,
+            },
             "url_is_canonical": self.url_is_canonical
         }
 
@@ -111,13 +124,19 @@ class PageStatistics:
         return res
 
     def get_fingerprint(self):
-        raw_data = [self.language, self.url_length, self.words_in_h1, self.words_in_h2, self.words_in_h3,
-                    self.nr_of_links_to_gov, self.nr_of_links_from_edu, self.nr_of_links_to_org]
-        return self.get_fingerprint_from_raw_data(raw_data)
 
-    def get_fingerprint_from_raw_data(self, raw_data):
-        string = ''.join(map(str, raw_data))
-        return fp(string)
+        raw_data = [self.language, self.url_length,
+                    self.words_in_h1,
+                    self.words_in_h2,
+                    self.words_in_h3,
+                    self.nr_of_links_from_gov,
+                    self.nr_of_links_from_edu,
+                    self.nr_of_links_from_org,
+                    self.nr_of_links_to_gov,
+                    self.nr_of_links_to_edu,
+                    self.nr_of_links_to_org]
+
+        return get_fingerprint_from_raw_data(raw_data)
 
     def get_nr_of_links_to_gov_org_edu(self, page):
         links_to_gov = 0
@@ -146,10 +165,16 @@ class PageStatistics:
 
         old_data = page_statistics.find_one({"page_id": self.page_id})
         if old_data:
-            old_fingerprint_data = [old_data['language'], old_data['url_length'], old_data['words_in_heading1'],
-                                    old_data['words_in_heading2'], old_data['words_in_heading3'],
-                                    old_data['nr_of_links_to_gov'],
-                                    old_data['nr_of_links_to_edu'], old_data['nr_of_links_to_org']]
+            old_fingerprint_data = [old_data['language'], old_data['url_length'],
+                                    old_data['words_in_headings']['heading1'],
+                                    old_data['words_in_headings']['heading2'],
+                                    old_data['words_in_headings']['heading3'],
+                                    old_data['nr_links_from_GEO']['from_gov'],
+                                    old_data['nr_links_from_GEO']['from_edu'],
+                                    old_data['nr_links_from_GEO']['from_org'],
+                                    old_data['nr_links_to_GEO']['to_gov'],
+                                    old_data['nr_links_to_GEO']['to_edu'],
+                                    old_data['nr_links_to_GEO']['to_org']]
 
             new_fingerprint = self.get_fingerprint()
 
@@ -193,3 +218,30 @@ class PageStatistics:
                 "ORG": compress_urls(named_entities_for_divs.get("ORG", None))
             }
         }
+
+    def get_page_rank_by_page_id(self, page_id, client):
+        db = client.get_database("Analytics")
+        page_statics = db['page_statistics']
+
+        current_analytics_data = page_statics.find_one({"_id": 0, "scores": 1}, {"page_id": page_id})
+        if current_analytics_data:
+            return current_analytics_data['scores'].get('pageRank')
+        return 0
+
+    def get_hub_score_by_page_id(self, page_id, client):
+        db = client.get_database("Analytics")
+        page_statics = db['page_statistics']
+
+        current_analytics_data = page_statics.find_one({"_id": 0, "scores": 1}, {"page_id": page_id})
+        if current_analytics_data:
+            return current_analytics_data['scores'].get('hub_score')
+        return 0
+
+    def get_authority_score_by_page_id(self, page_id, client):
+        db = client.get_database("Analytics")
+        page_statics = db['page_statistics']
+
+        current_analytics_data = page_statics.find_one({"_id": 0, "cores": 1}, {"page_id": page_id})
+        if current_analytics_data:
+            return current_analytics_data['scores'].get('authority_score')
+        return 0
